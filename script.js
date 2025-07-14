047bd9114a9edeb5b9c07e8a92d1723eb7c56d74
Sanjay7777776 <sanju001818@gmail.com>
2025-07-14 10:42:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
-function submit(){
-    debugger
-    var fname=document.getElementById('fname')
-    var lname=document.getElementById('lname')
-    var Fullname=fname.value+" "+lname.value
-    console.log(Fullname)
-    var email=document.getElementById('email')
-    var Address=document.getElementById('Address')
-    var emailAddress=email.value+" "+Address.value
-    console.log(emailAddress)
-}
+function submit() {
+
+    var fullname = document.getElementById('fullname');
+    console.log(fullname.value);
+
+    var phno = document.getElementById('phno');
+    console.log(phno.value);
+
+    var email = document.getElementById('email');
+    console.log(email.value);
+
+   
+    var selectedFood = "";
+    var selectedfoodRadio = document.querySelector('input[name="food"]:checked');
+    if (selectedfoodRadio) {
+        selectedFood = selectedfoodRadio.value;
+    }
+
+    console.log("Selected Food Preference:", selectedFood);
+    document.getElementById('selectedValue')?.remove();
+
+    var selectedtshirt = "";
+    var selectedtshirtRadio = document.querySelector('input[name="tshirt"]:checked');
+    if (selectedtshirtRadio) {
+        selectedtshirt = selectedtshirtRadio.value;
+    }
+
+    console.log("Selected tshirt :", selectedtshirt);
+    document.getElementById('selectedValue')?.remove(); 
+
+
+    var selectedgender = "";
+    var selectedgenderRadio = document.querySelector('input[name="gender"]:checked');
+    if (selectedgenderRadio) {
+        selectedgender = selectedgenderRadio.value;
+    }
+
+    console.log("Selected gender :", selectedgender);
+    document.getElementById('selectedValue')?.remove(); 
+  }
